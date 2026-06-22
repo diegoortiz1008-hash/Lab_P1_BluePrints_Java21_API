2c356c4dd5813d655295011071e94c4f548ac19e
diegoortiz1008-hash <diegoortiz1008@gmail.com>
2026-06-22 18:44:04 -0500

    Anotations to persistence

--- src/main/java/edu/eci/arsw/blueprints/model/Blueprint.java
+++ src/main/java/edu/eci/arsw/blueprints/model/Blueprint.java
@@ -5,17 +5,42 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "blueprints",
+       uniqueConstraints = @UniqueConstraint(columnNames = {"author", "name"}))
 public class Blueprint {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
     private String author;
     private String name;
+
+    @ElementCollection
+    @CollectionTable(name = "blueprint_points",
+                     joinColumns = @JoinColumn(name = "blueprint_id"))
     private final List<Point> points = new ArrayList<>();
+
+    protected Blueprint() {}
 
     public Blueprint(String author, String name, List<Point> pts) {
         this.author = author;
         this.name = name;
         if (pts != null) points.addAll(pts);
     }
+
 
     public String getAuthor() { return author; }
     public String getName() { return name; }
